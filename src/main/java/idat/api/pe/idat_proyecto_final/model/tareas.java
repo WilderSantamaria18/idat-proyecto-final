4c6ebee54cbeb3f459ff84a7d053dfcf6fe4c961
wilder <ln60763412@idat.pe>
2026-02-19 21:23:01 -0500

    si

---
+++ src/main/java/idat/api/pe/idat_proyecto_final/model/tareas.java
@@ -0,0 +1,5 @@
+package idat.api.pe.idat_proyecto_final.model;
+
+public class tareas {
+    
+}
--- src/main/java/idat/api/pe/idat_proyecto_final/model/usuario.java
+++ src/main/java/idat/api/pe/idat_proyecto_final/model/usuario.java
@@ -1,7 +1,5 @@
 package idat.api.pe.idat_proyecto_final.model;
 
-import java.sql.Date;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,6 +7,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+import java.time.LocalDateTime;
+import jakarta.persistence.PrePersist;
 
 @Entity
 @Table(name = "usuario")
@@ -23,6 +23,13 @@ public class usuario {
     private String password;
     private String rol;
 
-    @Column(name = "fecha_creacion")
-    private Date fechaCreacion;
+    @Column(name = "fecha_creacion", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP", nullable = false)
+    private LocalDateTime fechaCreacion;
+
+    @PrePersist
+    public void prePersist() {
+        if (fechaCreacion == null) {
+            fechaCreacion = LocalDateTime.now();
+        }
+    }
 }
