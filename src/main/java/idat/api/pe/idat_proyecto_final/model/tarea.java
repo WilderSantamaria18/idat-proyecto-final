21056928a4298cc77f872ed7ccd0b583114bd4e3
wilder <ln60763412@idat.pe>
2026-02-19 21:28:22 -0500

    Actualizar modelo de tarea con mejoras en estructura y anotaciones

--- src/main/java/idat/api/pe/idat_proyecto_final/model/tarea.java
+++ src/main/java/idat/api/pe/idat_proyecto_final/model/tarea.java
@@ -3,6 +3,8 @@ package idat.api.pe.idat_proyecto_final.model;
 import java.time.LocalDate;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -11,11 +13,11 @@ import lombok.Data;
 @Data
 public class tarea {
     
-
-
+    @Id 
+    @GeneratedValue(strategy = generationType.IDENTITY)
     private Long id;
     private String titulo;
     private String descripcion;
     private LocalDate fechalimite;
-    
+
 }
